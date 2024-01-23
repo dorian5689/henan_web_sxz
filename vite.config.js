3698cbaff0196320c55ce38197ca019195d06926
wuhao <1069461929@qq.com>
2024-01-23 15:46:57 +0800

    引入element-plus

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,24 @@
-import vue from '@vitejs/plugin-vue'
-import {defineConfig} from 'vite'
-import AutoImport from 'unplugin-auto-import/vite'
+// vite.config.js
+import {defineConfig} from 'vite';
+import vue from '@vitejs/plugin-vue';
 import Components from 'unplugin-vue-components/vite'
 import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
-import ElementPlus from 'unplugin-element-plus/vite'
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [vue(),
-            AutoImport({
-                    resolvers: [ElementPlusResolver()],
-                              }),
-            Components({
-            resolvers: [ElementPlusResolver()],
-                            }),
-            [ElementPlus()],
-            ],
 
-})
+// defineConfig 默认配置
+// Components 组件解析resolvers的 ElementPlusResolver()
+// 记得带括号()
+
+export default defineConfig(
+    {
+    plugins: [vue(),
+        Components(
+            {
+                resolvers: [ElementPlusResolver()]
+            })
+    ],
+    server: {
+        // 自动打开浏览器
+        open: false
+    }
+});
