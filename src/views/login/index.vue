2b1bd67642ff71a120ddd66449006b927f2406d2
wuhao <1069461929@qq.com>
2024-01-23 16:16:23 +0800

    setup 语法糖

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,18 +1,88 @@
-<script setup>
-// 响应式对象 reactive,之后会有ref
+
+<!-- 这里是大于3.2-->
+<script  setup>
+// 响应式对象 reactive,defineComponent 对整个对象更好的类型提示,TS
 import {reactive} from "vue";
-// 之后模版展示数据
-const obj = reactive({name:'登录'})
+
+// 不要要包含到 state中
+const obj = reactive({id:1})
+const  arr = reactive(      [
+          {title:'标题',id:1},
+          {title:'标题',id:2},
+          {title:'标题',id:3},
+      ])
+
+// 没有state
+const  add=()=>{
+  arr.push({title: '标题',
+  id:arr.length+1})
+}
+// 不需要返回了
 </script>
 
 <template>
   <h1>
-      {{obj.name}}
+    {{obj.id}}
   </h1>
-  <el-button type="primary">Primary</el-button>
+  <ul>
+    <li v-for="(v,i) in arr":key="v.id">{{v.title}}
+    </li>
+  </ul>
+  <el-button type="primary" @click="add">添加数据</el-button>
 
 </template>
 
 <style scoped>
 
 </style>
+
+
+<!-- 这里是小于3.2 没有setup
+
+<script >
+// 响应式对象 reactive,defineComponent 对整个对象更好的类型提示,TS
+import {reactive,defineComponent} from "vue";
+export  default defineComponent({
+  setup(){
+    // 创建响应式数据对象
+    const state = reactive({
+      id:1,
+      arr:[
+          {title:'标题',id:1},
+          {title:'标题',id:2},
+          {title:'标题',id:3},
+      ]
+    });
+    const  add=()=>{
+      state.arr.push({title: '标题',
+      id:state.arr.length+1})
+    }
+    // 创建后返回
+    //setup函数中将响应式数据对象return出去供template使用
+    // 返回对象和函数,....可扩展运算符
+    return {...state,
+    add
+    };
+
+
+  }
+})
+</script>
+
+<template>
+  <h1>
+    {{id}}
+  </h1>
+  <ul>
+    <li v-for="(v,i) in arr":key="v.id">{{v.title}}
+    </li>
+  </ul>
+  <el-button type="primary" @click="add">添加数据</el-button>
+
+</template>
+
+<style scoped>
+
+</style>
+
+-->
