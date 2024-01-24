6753a83d55572fef627ff7dd7c553f63619ca2a8
wuhaohao <1069461929@qq.com>
2024-01-25 00:05:01 +0800

    登录后跳转

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,15 +2,33 @@
 // 复制login/index.vue
 // 响应式对象 reactive,之后会有ref
 import {reactive} from "vue";
+import {useRouter} from "vue-router";
+
+import NavMeun from "./navMeun.vue";
 // 之后模版展示数据
+const  router = useRouter()
 const obj = reactive({name:'首页'})
+const  goBack =()=>{
+  router.push('/login') // 跳转到登录
+}
 </script>
 
 <template>
-  <h1>
-    1233333333
-    {{obj.name}}
-  </h1>
+  <div id="module">
+    <el-container>
+      <NavMeun></NavMeun>
+      <!-- 左侧导航-->
+    <!--<el-aside width="200px">Aside</el-aside>-->
+      <el-container>
+        <!--右侧头部-->
+        <el-header >
+           <el-button type="primary" class="exit" @click="goBack"> 退出</el-button>
+         </el-header>
+        <!--右侧主体-->
+        <el-main>666666</el-main>
+      </el-container>
+    </el-container>
+  </div>
 
 </template>
 
---
+++ src/views/home/navMeun.vue
@@ -0,0 +1,36 @@
+<script setup>
+import {reactive} from "vue";
+import {
+  Document,
+  Menu as IconMenu,
+  Location,
+  Setting,
+} from '@element-plus/icons-vue'
+
+const  items = reactive([
+  {name:"首页",icon:Document,url:'/index'},
+  {name:"电量监控",icon:Location,url:'/index2'},
+  {name:"统计分析",icon:Setting,url:'/index3'},
+])
+</script>
+
+<template>
+    <!-- 左侧导航-->
+    <el-aside width="300px"></el-aside>
+      <el-menu
+        default-active="2"
+        class="el-menu-vertical-demo">
+        <el-sub-menu index="v.url" v-for="v in items" :key="v.url">
+          <template #title>
+            <el-icon><component :is="v.icon"></component></el-icon>
+            <span>{{v.name}}</span>
+          </template>
+          </el-sub-menu>
+
+      </el-menu>
+
+</template>
+
+<style scoped>
+
+</style>
